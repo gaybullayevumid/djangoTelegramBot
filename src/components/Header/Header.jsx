1ff25f04a42a76d59f4e95a07d15bdf5b4b89622
G'aybullayev Umid <123540083+gaybullayevumid@users.noreply.github.com>
2024-07-10 16:54:25 +0500

    init: app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,24 +4,35 @@ import './Header.css'
 
 export default function Header() {
     return (
-        <div className='navbar'>
-            <div className="logo">
-                <Link to={"/"}>umid-g'aybullayev</Link>
+        <>
+            <div className='navbar'>
+                <div className="logo">
+                    <Link to={"/"}>umid-g'aybullayev</Link>
+                </div>
+                <ul>
+                    <li className='active'>
+                        <Link to={"/"}>_hello</Link>
+                    </li>
+                    <li>
+                        <Link to={"/about"}>_about-me</Link>
+                    </li>
+                    <li>
+                        <Link to={"/projects"}>_projects</Link>
+                    </li>
+                    <li>
+                        <Link to={"/contact"}>_contact-me</Link>
+                    </li>
+                </ul>
             </div>
-            <ul>
-                <li className='active'>
-                    <Link to={"/"}>_hello</Link>
-                </li>
-                <li>
-                    <Link to={"/about"}>_about-me</Link>
-                </li>
-                <li>
-                    <Link to={"/projects"}>_projects</Link>
-                </li>
-                <li>
-                    <Link to={"/contact"}>_contact-me</Link>
-                </li>
-            </ul>
-        </div>
+
+
+            <div className="mobile-navbar">
+                <div className="logo">
+                    <Link to={"/"}>umid-g'aybullayev</Link>
+                </div>
+
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </>
     )
 }
