dddd3add5a8fd54d0bdb48ec38ed5d8fd46914eb
G'aybullayev Umid <123540083+gaybullayevumid@users.noreply.github.com>
2024-07-12 09:33:25 +0500

    init: app

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Home from './components/Home/Home'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
-import './App.css'
 import Footer from './components/Footer/Footer'
+import './App.css'
 
 export default function App() {
   return (
