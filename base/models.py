857b2366021e5210da767c98be35dd55ff37405a
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-10 12:28:49 +0500

    feat: create model

--- base/models.py
+++ base/models.py
@@ -1,8 +1,9 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
 
 class User(models.Model):
     username = models.CharField(max_length=50)
-
-
+    phone = models.CharField(max_length=15, validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$',)])
+    image = models.ImageField(upload_to='images/')
