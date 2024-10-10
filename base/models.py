291ce7b67b344d0169a8b95cf675d0bd39c61caf
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-10 13:14:29 +0500

    feat: create functions in models

--- base/models.py
+++ base/models.py
@@ -7,3 +7,7 @@ class User(models.Model):
     username = models.CharField(max_length=50)
     phone = models.CharField(max_length=15, validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$',)])
     image = models.ImageField(upload_to='images/')
+
+
+    def __str__(self):
+        return self.username
