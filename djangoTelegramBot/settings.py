8439730a4a42cef257b9cfbd7aa63dd2e0c77e37
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-10 13:15:47 +0500

    feat: install rest framework and connect settings

--- djangoTelegramBot/settings.py
+++ djangoTelegramBot/settings.py
@@ -25,6 +25,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # packages
+    'rest_framework',
+
+    # local apps
     'base.apps.BaseConfig'
 ]
 
